05f0f91342799f8586ff7a846c882a8430c35c35
LittleTruck <i839855314i@gmail.com>
2021-06-12 16:43:43 +0800

    change router mode

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/nccu-dist-zookeeper-frontend/'
-        : '/nccu-dist-zookeeper-frontend/',
+        : '/nccu-dist-zookeeper-frontend/', 
+    css: {
+        extract: false
+    }
 }
